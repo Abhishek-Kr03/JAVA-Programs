0b200bcb6666e7732ce1edfcbea9049de7dc8b28
Abhishek Kumar <166202255+abhishek-kr03@users.noreply.github.com>
2024-05-15 20:31:42 +0530

    Converting degree from celsius to faherenheit

--- datatypes/CelsiusToFaherenheit.java
+++ datatypes/CelsiusToFaherenheit.java
@@ -6,11 +6,10 @@ public class CelsiusToFaherenheit {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the celsius value:");
-		double celsius = scan.nextInt();
+		double celsius = scan.nextDouble();
 		double faherenheit = (celsius * 9/5) + 32;
 		System.out.println("The faherenheit value is:");
 		System.out.printf("%.4f" , faherenheit);
-		
+		scan.close();
 	}
-
 }
